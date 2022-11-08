a51612f3459c72f3f8e1ea9e4f18527143a717e2
Oleksii AnurovPrykhodko <anyrov.prihodko@gmail.com>
2022-11-08 12:41:42 +0200

    add my user name to package.json

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,26 +1,23 @@
+import FriendListItem from './FriendListItem';
 import PropTypes from 'prop-types';
 
 function FriendList({ friends }) {
   return (
     <ul className="friend-list">
       {friends.map(friend => (
-        <li key={friend.id} className="item">
-          <span className="status">{friend.isOnline ? '🟢' : '🔴'}</span>
-          <img
-            className="avatar"
-            src={friend.avatar}
-            alt={`${friend.name} avatar`}
-            width="48"
-          />
-          <p className="name">{friend.name}</p>
-        </li>
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
       ))}
     </ul>
   );
 }
 
 FriendList.propTypes = {
-  friends: PropTypes.array.isRequired,
+  friends: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
 };
 
 export default FriendList;
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Statistics({ title, stats }) {
   return (
     <section className="statistics">
-      <h2 className="title">{title && title}</h2>
+      {title && <h2 className="title">{title}</h2>}
 
       <ul className="stat-list">
         {stats.map(item => (
